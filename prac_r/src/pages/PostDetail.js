dfaecc8394fb042a1790daaf87bf5788ba2f4d41
neneee0181 <2022180024@tukorea.ac.kr>
2025-02-23 18:01:11 +0900

    게시글 목록 가져오기 & detail

--- prac_r/src/pages/PostDetail.js
+++ prac_r/src/pages/PostDetail.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useParams, Link } from 'react-router-dom';
 import './../css/PostDetail.css';
+import axios from "axios";
 
 function PostDetail() {
     const { id } = useParams();
 
-    const post = {
-        id,
-        title: `${id}번째 게시글`,
-        content: `${id}번째 게시글의 상세 내용입니다. 이곳에 게시글 내용이 들어갑니다.`,
+    let [post,setPost] = useState({
+        title:"",
+        content:""
+    });
+
+    const getPost= () => {
+        axios.get(`${process.env.REACT_APP_API_URL}/posts/${id}`)
+            .then(response => {
+                console.log('게시글 가져오기 성공:', response.data);
+                setPost(response.data);
+            })
+            .catch(error => {
+                console.error('게시글 가져오기 실패:', error);
+            });
     };
+
+    useEffect(()=>{
+        getPost();
+    },[]);
 
     const handleDelete = () => {
         alert('게시글이 삭제되었습니다.');
