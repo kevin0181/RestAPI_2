774fbbbb9b2772ef430762306488783b274d5b79
neneee0181 <2022180024@tukorea.ac.kr>
2025-02-23 17:39:49 +0900

    게시글 작성 완

--- prac_r/src/pages/CreatePost.js
+++ prac_r/src/pages/CreatePost.js
@@ -1,21 +1,52 @@
-import React from 'react';
 import './../css/CreatePost.css';
+import {useState} from "react";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 function CreatePost() {
+
+    let navigate = useNavigate();
+
+    let [formData, setFormData] = useState({
+        title: '',
+        content: ''
+    });
+
+    let onChangeFormData = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const onClickSubmit = () => {
+        axios.post(`${process.env.REACT_APP_API_URL}/posts`, formData)
+            .then(response => {
+                console.log('게시글 등록 성공:', response.data);
+                alert("게시글이 등록되었습니다.");
+                navigate('/');
+            })
+            .catch(error => {
+                console.error('게시글 등록 실패:', error);
+                alert("게시글 등록에 실패하였습니다.");
+
+            });
+    };
+
     return (
         <div className="create-post-container">
             <h1 className="create-post-title">게시글 작성</h1>
-            <form className="create-post-form">
+            <div className="create-post-form">
                 <div className="form-group">
                     <label htmlFor="title">제목</label>
-                    <input id="title" type="text" placeholder="제목을 입력하세요" />
+                    <input id="title" name="title" type="text" placeholder="제목을 입력하세요" onChange={onChangeFormData}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="content">내용</label>
-                    <textarea id="content" placeholder="내용을 입력하세요" />
+                    <textarea id="content" name="content" placeholder="내용을 입력하세요" onChange={onChangeFormData}/>
                 </div>
-                <button type="submit" className="submit-button">등록</button>
-            </form>
+                <button type="button" className="submit-button" onClick={onClickSubmit}>등록</button>
+            </div>
         </div>
     );
 }
